002409c62d85af6c3504537e92f0c7db833720ae
Michael Crilly <mcrilly@skyscapecloud.com>
2014-01-23 12:55:16 +0000

    Adding standard API classes with a meaningful JSON return object for get request type.

--- vsvlandb/api_endpoints.py
+++ vsvlandb/api_endpoints.py
@@ -4,9 +4,49 @@ from flask import render_template
 from flask.ext import restful
 
 # Define our endpoints:
-class HelloWorld(restful.Resource):
+class VLANs(restful.Resource):
+
     def get(self):
-        return render_template('base.html', name='Michael')
+        return {'get': 'Not implemented'}
+
+    def post(self):
+    	return {'post': 'Not implemented'}
+
+	def put(self):
+		return {'put': 'Not implemented'}
+
+	def delete(self):
+		return {'delete': 'Not implemented'}
+
+class Subnets(restful.Resource):
+
+    def get(self):
+        return {'get': 'Not implemented'}
+
+    def post(self):
+    	return {'post': 'Not implemented'}
+
+	def put(self):
+		return {'put': 'Not implemented'}
+
+	def delete(self):
+		return {'delete': 'Not implemented'}
+
+class Sites(restful.Resource):
+
+    def get(self):
+        return {'get': 'Not implemented'}
+
+    def post(self):
+    	return {'post': 'Not implemented'}
+
+	def put(self):
+		return {'put': 'Not implemented'}
+
+	def delete(self):
+		return {'delete': 'Not implemented'}
 
 # Add them to the API:
-api.add_resource(HelloWorld, '/api/')
+api.add_resource(VLANs, '/api/vlans/')
+api.add_resource(Subnets, '/api/subnets/')
+api.add_resource(Sites, '/api/sites/')
