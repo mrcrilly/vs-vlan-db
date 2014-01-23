0e95bf20178593b9486e457656adaf700e5b8341
Michael Crilly <mcrilly@skyscapecloud.com>
2014-01-23 09:24:28 +0000

    Added basic routes to produce static output from templates

--- vsvlandb/web_endpoints.py
+++ vsvlandb/web_endpoints.py
@@ -9,8 +9,24 @@ def index():
 
 @app.route('/vlans')
 def vlans():
-	return render_template('vlans.html')
+	return render_template('vlans_list.html')
+
+@app.route('/vlans/add')
+def vlans_add():
+	return render_template('vlans_add.html')
+
+@app.route('/subnets')
+def subnets():
+	return render_template('subnets_list.html')
+
+@app.route('/subnets/add')
+def subnets_add():
+	return render_template('subnets_add.html')
 
 @app.route('/sites')
 def sites():
-	return render_template('sites.html')
+	return render_template('sites_list.html')
+
+@app.route('/sites/add')
+def sites_add():
+	return render_template('sites_add.html')
