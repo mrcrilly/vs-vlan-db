9c34429c5c57687d76809bca3806653e73acfcce
Michael Thomas Crilly <m@mcgb.io>
2014-01-23 22:53:51 +0000

    Added some more data to database_initialise

--- database_initialise.py
+++ database_initialise.py
@@ -1,9 +1,26 @@
 from vsvlandb.models import VLAN,Subnet,Site
 from vsvlandb import dbo
-site = Site('A9')
-subnet = Subnet('10.8.1.0', '255.255.255.0', site)
-vlan = VLAN('1024', subnet, site)
-dbo.session.add(site)
-dbo.session.add(subnet)
-dbo.session.add(vlan)
+
+site_london = Site('London')
+site_paris = Site('Paris')
+
+subnet_1 = Subnet('10.8.0.0', '255.255.0.0', site_london)
+subnet_2 = Subnet('10.8.0.0', '255.255.0.0', site_paris)
+subnet_3 = Subnet('192.168.1.0', '255.255.255.0', site_london)
+
+vlan_1 = VLAN('15', subnet_1, site_london)
+vlan_2 = VLAN('99', subnet_2, site_london)
+vlan_3 = VLAN('456', subnet_3, site_paris)
+
+dbo.session.add(site_london)
+dbo.session.add(site_paris)
+
+dbo.session.add(subnet_1)
+dbo.session.add(subnet_2)
+dbo.session.add(subnet_3)
+
+dbo.session.add(vlan_1)
+dbo.session.add(vlan_2)
+dbo.session.add(vlan_3)
+
 dbo.session.commit()
