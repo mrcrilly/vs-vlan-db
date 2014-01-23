640a0f8eafc6c0fb554cffb80382b94932d0a95c
Michael Crilly <mcrilly@skyscapecloud.com>
2014-01-23 12:55:42 +0000

    Updated to include new templates and methods

--- vsvlandb/web_endpoints.py
+++ vsvlandb/web_endpoints.py
@@ -3,10 +3,14 @@ from vsvlandb import app
 
 from flask import request, render_template
 
+# Root/Index
 @app.route('/')
 def index():
 	return render_template('index.html')
 
+
+
+# VLANS
 @app.route('/vlans')
 def vlans():
 	return render_template('vlans_list.html')
@@ -15,6 +19,17 @@ def vlans():
 def vlans_add():
 	return render_template('vlans_add.html')
 
+@app.route('/vlans/edit')
+def vlans_edit():
+	return render_template('vlans_edit.html')
+
+@app.route('/vlans/delete')
+def vlans_delete():
+	return render_template('vlans_delete.html')
+
+
+
+# Subnets
 @app.route('/subnets')
 def subnets():
 	return render_template('subnets_list.html')
@@ -23,6 +38,17 @@ def subnets():
 def subnets_add():
 	return render_template('subnets_add.html')
 
+@app.route('/subnets/edit')
+def subnets_edit():
+	return render_template('subnets_edit.html')
+
+@app.route('/subnets/delete')
+def subnets_delete():
+	return render_template('subnets_delete.html')
+
+
+
+#Sites
 @app.route('/sites')
 def sites():
 	return render_template('sites_list.html')
@@ -30,3 +56,11 @@ def sites():
 @app.route('/sites/add')
 def sites_add():
 	return render_template('sites_add.html')
+
+@app.route('/sites/edit')
+def sites_edit():
+	return render_template('sites_edit.html')
+
+@app.route('/sites/delete')
+def sites_delete():
+	return render_template('sites_delete.html')
