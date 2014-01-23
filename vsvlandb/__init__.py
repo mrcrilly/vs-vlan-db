588ab1a5df23d3a76392f970aefb548889ff6595
Michael Crilly <mcrilly@skyscapecloud.com>
2014-01-23 14:20:28 +0000

    Setting up dbo object

--- vsvlandb/__init__.py
+++ vsvlandb/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
-from flask.ext import restful
+from flask.ext import restful, sqlalchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/vlan.db'
+
 api = restful.Api(app)
+dbo = sqlalchemy.SQLAlchemy(app)
 
 from vsvlandb import web_endpoints, api_endpoints
